5d00ee1013e6a5c3f5028b0516183bebeb232540
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2026-04-18 07:05:56 +0200

    perf: use React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, type Plugin } from 'vite'
+import babel from 'vite-plugin-babel'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter(), reactCompiler()],
   resolve: {
     tsconfigPaths: true,
   },
 })
+
+function reactCompiler(): Plugin {
+  const reactCompilerConfig = {
+    // @see https://react.dev/reference/react-compiler/configuration
+  }
+  return babel({
+    filter: /\.[jt]sx?$/,
+    babelConfig: {
+      presets: ['@babel/preset-typescript'], // if you use TypeScript
+      plugins: [['babel-plugin-react-compiler', reactCompilerConfig]],
+    },
+  })
+}
